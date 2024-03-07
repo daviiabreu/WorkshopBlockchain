73430f47369bff365a649a50752a75c225803538
Davi Abreu da Silveira <davi.silveira@sou.inteli.edu.br>
2024-03-07 17:51:52 -0300

    update

---
+++ workshop/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="border-2 border-red-500">
+      <h1 className="text-bold text-blue-300">Hello World!</h1>
+      <p className="text-sm text-white text-underline">Olá inteli</p>
+      
+    </div>
+  );
+}
+
+export default App;
